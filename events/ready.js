773c7f45ceb292091b9d026addc7f89b660d977a
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-07-17 20:01:58 +0200

    update ready

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,23 @@
+"use strict";
+
+const logo = require('asciiart-logo');
+
 module.exports = async client => {
 
-  client.logger.log(`${client.user.tag} est prête à servir ${client.users.size} utilisateurs dans ${client.guilds.size} serveurs.`, "ready");
+  await console.log(
+    logo({
+      name: `${client.user.username}`,
+      font: 'Basic',
+      lineChars: 15,
+      padding: 5,
+      margin: 2
+    })
+    .emptyLine()
+    .wrap(`${client.user.username}#${client.user.discriminator} développé par Kévin.`)
+    .render()
+  );
+  
+  client.logger.log(`${client.user.tag} est prête à servir ${client.users.size -1} utilisateurs dans ${client.guilds.size} serveurs.`, "ready");
 
   client.user.setActivity(`${client.settings.get("default").prefix}help`, {type: "PLAYING"});
   
