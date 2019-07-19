8b8124d22fc41684bef7371949acf383e07ef3ac
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-07-19 09:34:15 +0200

    Create emojies.js

---
+++ commands/emojies.js
@@ -0,0 +1,23 @@
+"use strict";
+
+exports.run = async (client, message) => {
+
+    if (message.guild.emojis.size == 0) return message.channel.send("<:warn:600349289427894272> Il n'y a pas d'emojis sur ce serveur.");
+
+    message.channel.send(`Voici les **${message.guild.emojis.filter(e => e.toString()).size}** emojies du serveur **${message.guild.name}**:\n${message.guild.emojis.map(e => e).join(' - ')}`)
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "emojies",
+  category: "Divers",
+  description: "Affiche les emojies du serveur.",
+  usage: "emojies"
+};
