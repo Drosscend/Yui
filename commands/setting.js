26353e08c7d166358be2f387be90cf30f0577df5
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-07-19 10:44:13 +0200

    Update setting.js

--- commands/setting.js
+++ commands/setting.js
@@ -22,11 +22,11 @@ exports.run = async (client, message, [action, key, ...value]) => {
   } else
   
   if (action === "reset") {
-    if (!key) return message.reply("<:warn:600349289427894272>Veuillez spécifiez une clée à réinitialiser.");
-    if (!settings[key]) return message.reply("<:warn:600349289427894272>Cette clé n'existe pas dans les réglages.");
-    if (!overrides[key]) return message.reply("<:warn:600349289427894272>Cette clée utilise déjà les valeurs par défaut.");
+    if (!key) return message.reply("<:warn:600349289427894272> Veuillez spécifiez une clée à réinitialiser.");
+    if (!settings[key]) return message.reply("<:warn:600349289427894272> Cette clé n'existe pas dans les réglages.");
+    if (!overrides[key]) return message.reply("<:warn:600349289427894272> Cette clée utilise déjà les valeurs par défaut.");
     
-    const response = await this.client.awaitReply(message, `<:warn:600349289427894272>Êtes-vous sûr de vouloir réinitialise \`${key}\` à la valeur par défaut \`${defaults[key]}\`?. Répondres par **oui** ou **non**`);
+    const response = await this.client.awaitReply(message, `<:warn:600349289427894272> Êtes-vous sûr de vouloir réinitialise \`${key}\` à la valeur par défaut \`${defaults[key]}\`?. Répondres par **oui** ou **non**`);
 
     if (["y", "yes", "oui"].includes(response.toLowerCase())) {
       client.settings.delete(message.guild.id, key);
