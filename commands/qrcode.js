52af3944dcb0b4edc46492a8ce8b5e3087e94603
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-07-23 16:59:56 +0200

    Create qrcode.js

---
+++ commands/qrcode.js
@@ -0,0 +1,31 @@
+"use strict";
+
+exports.run = async (client, message, args) => {
+
+    var word = args[0];
+    if(!word) return message.channel.send("<:warn:600349289427894272> Veuillez taper un mot!");
+
+    message.channel.send("Veuillez patientez").then(m => {
+        message.channel.send({
+            files: [{
+              attachment: `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${word}`,
+              name: `${word}.png` //.gif si c'est un gif
+            }]
+        }).then( () => m.delete());
+    });
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "qrcode",
+  category: "Fun",
+  description: "Affiche un QR Code avec votre mot !",
+  usage: "qrcode [texte]"
+};
