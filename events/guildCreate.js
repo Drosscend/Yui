879fbc5efe84a37492fbe38f17fc628aa87219a9
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-07-31 16:14:58 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,6 +2,6 @@
 
 module.exports = (client, guild) => {
 
-  client.logger.cmd(`[GUILD JOIN] ${guild.name} vient de m'ajouter. Propriétaire: ${guild.owner.user.tag}.`);
+  client.logger.debug(`[GUILD JOIN] ${guild.name} vient de m'ajouter. Propriétaire: ${guild.owner.user.tag}.`);
 
 };
