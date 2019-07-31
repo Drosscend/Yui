0e90508dab193bf3c272ad2d471ba8541a8f9f3e
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-07-31 16:15:00 +0200

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,7 +2,7 @@
 
 module.exports = (client, guild) => {
 
-  client.logger.cmd(`[GUILD LEAVE] ${guild.name} vient de m'éjecter.`);
+  client.logger.debug(`[GUILD LEAVE] ${guild.name} vient de m'éjecter.`);
 
   if (client.settings.has(guild.id)) {
     client.settings.delete(guild.id);
