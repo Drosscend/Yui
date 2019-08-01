d75f1a60e731ec99b9013d3c8b689bc31c207bd0
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-08-01 08:50:15 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,5 +3,5 @@
 module.exports = (client, guild) => {
 
   client.logger.debug(`[GUILD JOIN] ${guild.name} vient de m'ajouter. Propriétaire: ${guild.owner.user.tag}.`);
-  client.channels.get("606360729158680586").send(`[GUILD JOIN] **${guild.name}** vient de m'ajouter. Propriétaire: **${guild.owner.user.tag}**.`)
+  client.channels.get("606360729158680586").send(`[GUILD JOIN] **${guild.name}** (${guild.id}) vient de m'ajouter. Propriétaire: **${guild.owner.user.tag}**.`)
 };
