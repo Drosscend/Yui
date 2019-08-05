075ee4586a8eb5b88fbb1297612381efce0a8d90
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-08-05 10:12:18 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -35,11 +35,7 @@ module.exports = async (client, message) => {
     return message.channel.send("<:barrier:600349286546276362> Cette commande n'est pas disponible par message privé. Veuillez exécuter cette commande dans un serveur.");
 
   if (level < client.levelCache[cmd.conf.permLevel]) {
-    if (settings.systemNotice === "true") {
       return message.channel.send(`<:forbidden:600349288823783449> Vous n'avez pas la permission d'utiliser cette commande, votre niveau de privilège est **${level}**, \`${client.config.permLevels.find(l => l.level === level).name}\` et le niveau de privilère demandé est **${client.levelCache[cmd.conf.permLevel]}** \`${cmd.conf.permLevel}\`.`);
-    } else {
-      return;
-    }
   }
 
   message.author.permLevel = level;
