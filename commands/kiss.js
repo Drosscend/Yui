8c325f211c6aef5ca0ca707e00c596b6da14db13
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-08-24 16:47:57 +0200

    Create kiss.js

---
+++ commands/kiss.js
@@ -0,0 +1,69 @@
+"use strict";
+const { get } = require("https");
+const talkedRecently = new Set();
+
+exports.run = async (client, message, args) => {
+
+    if (talkedRecently.has(message.author.id)) {
+        return message.channel.send(`${message.author} Attendez 10 secondes avant de taper à nouveau ce qui suit`);
+    }
+
+    get("https://neko-love.xyz/api/v1/kiss", (res) => {
+        const { statusCode } = res;
+        if (statusCode != 200) {
+            res.resume;
+        }
+        res.setEncoding("utf8");
+        let rawData = '';
+        res.on("data", (chunk) => {
+            rawData += chunk;
+        });
+        res.on("end", () => {
+            try {
+                const parsedData = JSON.parse(rawData);
+                message.channel.send({
+                    embed: {
+                        color: 0xDF9C9D,
+                        author: {
+                            name: message.author.username,
+                            icon_url: message.author.displayAvatarURL
+                        },
+                        footer: {
+                            icon_url: client.user.displayAvatarURL,
+                            text: client.user.username
+                        },
+                        timestamp: new Date(),
+                        description: `<:picture:605752181173256202> Image **kiss** générée par **neko-love.xyz**: [Lien de l'image](${parsedData.url})`,
+                        image: {
+                            url: parsedData.url
+                        },
+                    }
+                })
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    }).on("error", (err) => {
+        console.error(err.message);
+    });
+
+    talkedRecently.add(message.author.id);
+    setTimeout(() => {
+
+        talkedRecently.delete(message.author.id);
+    }, 10000);
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "kiss",
+    category: "Images",
+    description: "Affiche une image de type\"kiss\"",
+    usage: "kiss"
+};
